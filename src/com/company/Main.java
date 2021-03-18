2218b3e47fc1a3ef1dafa838ce02950780591571
Chris Almeida <chris@gem.co>
2021-03-18 11:12:59 -0600

    get base url from env

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         String apiKey = System.getenv("GEM_API_KEY");
         String secret = System.getenv("GEM_API_SECRET");
-        Gem gem = new Gem("https://api.sandbox.gem.co", apiKey, secret);
+        String baseURL = System.getenv("GEM_API_URL");
+        Gem gem = new Gem(baseURL, apiKey, secret);
         gem.getTransaction("1234");
     }
 }
